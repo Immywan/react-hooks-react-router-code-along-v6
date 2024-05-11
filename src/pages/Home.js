3740195fbe27ed32d2b1d4e572c7b6954b82acf6
Immaculatewangari <immaculate.wangari@student.moringaschool.com>
2024-05-11 18:06:59 +0300

    Complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
   const [users, setUsers] = useState([])
-
   useEffect(() =>{
     fetch("http://localhost:4000/users")
       .then(r => r.json())
       .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
+      .catch(error => console.error(error))
+  }, [])
+
   const userList = users.map(user =>{
     return <UserCard key={user.id} user={user}/>
   });
@@ -18,7 +18,7 @@ function Home() {
   return (
     <>
       <header>
-        {/* place NavBar here */}
+      <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
@@ -27,5 +27,5 @@ function Home() {
     </>
   );
 };
-
 export default Home;
+  
